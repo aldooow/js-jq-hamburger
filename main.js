e1831fce408d5497b8e3244b973d57a9a657dcc6
Aldo Cahuana <aldooow@icloud.com>
2020-06-08 18:10:55 +0200

    Fix!!

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 /* ---> METODO 1*/
 
-// $('a').click(function(){
-//   $('.hamburger-menu').addClass('active');
-// });
-// $('a.close').click(function(){
-//   $('.hamburger-menu').removeClass('active');
-// });
+$('.header-right a').click(function(){
+  $('.hamburger-menu').addClass('active');
+});
+$('.hamburger-menu a.close').click(function(){
+  $('.hamburger-menu').removeClass('active');
+});
 
 /* ---> METODO 2*/
-var variabile = false;
-
-$('a').click(function(){
-  if(variabile == false){
-    $('.hamburger-menu').addClass('active');
-    variabile = true;
-  } else {
-    $('.hamburger-menu').removeClass('active')
-    variabile = false;
-  }
-})
+// var variabile = false;
+//
+// $('a').click(function(){
+//   if(variabile == false){
+//     $('.hamburger-menu').addClass('active');
+//     variabile = true;
+//   } else {
+//     $('.hamburger-menu').removeClass('active')
+//     variabile = false;
+//   }
+// })
